5baf3673b8e49c6ac81972127ed1cc379ff1d5c0
Ivo Todorov <ivo2291@gmail.com>
2022-11-16 16:35:46 +0200

    Add files via upload

--- 02. conditional_statements/exercise/pets.py
+++ 02. conditional_statements/exercise/pets.py
@@ -1,19 +1,25 @@
 import math
 
 days_count = int(input())
-left_food_in_kg = int(input())
-dog_day_food_in_kg = float(input())
-cat_day_food_in_kg = float(input())
-turtle_day_food_in_gr = float(input())
+food_left_kg = int(input())
+dog_day_food_kg = float(input())
+cat_day_food_kg = float(input())
+turtle_day_food_grams = float(input())
 
-dog_food_needed = days_count * dog_day_food_in_kg
-cat_food_needed = days_count * cat_day_food_in_kg
-turtle_food_needed = (days_count * turtle_day_food_in_gr) / 1000
-total_food = dog_food_needed + cat_food_needed + turtle_food_needed
-difference = abs(left_food_in_kg - total_food)
+turtle_day_food_kg = turtle_day_food_grams / 1000
+food_is_enough = False
 
-if total_food <= left_food_in_kg:
+food_eaten_by_dog = days_count * dog_day_food_kg
+food_eaten_by_cat = days_count * cat_day_food_kg
+food_eaten_by_turtle = days_count * turtle_day_food_kg
+total_eaten_food = food_eaten_by_dog + food_eaten_by_cat + food_eaten_by_turtle
+
+difference = abs(food_left_kg - total_eaten_food)
+
+if food_left_kg >= total_eaten_food:
+    food_is_enough = True
+
+if food_is_enough:
     print(f"{math.floor(difference)} kilos of food left.")
-
 else:
     print(f"{math.ceil(difference)} more kilos of food are needed.")
